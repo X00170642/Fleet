c59f71619b48a0aa7ae5ffe8995cdb9c57732c36
JakeW <X00170642@myTUDublin.ie>
2023-02-06 16:53:38 +0000

    Add project files.

---
+++ Fleet/Models/Car.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Fleet.Models
+{
+    public class Car
+    {
+
+
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Make cannot be empty;");
+                }
+                make = value;
+            }
+        }
+
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Model cannot be empty;");
+                }
+                model = value;
+            }
+        }
+
+        
+        private string registration;
+
+
+        public string Registration
+        {
+            get { return registration; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Registration cannot be empty;");
+                }
+                registration = value;
+            }
+        }
+
+
+        public int EngineSize { get; set; } 
+
+        
+
+
+
+        public Car(string make, string model, string registration, int enginesize)
+        {
+            this.make = make;
+            this.model = model;
+            this.registration = registration;
+            this.EngineSize= enginesize;
+
+        }
+
+        public override string ToString()
+        {
+            return "Make: " + Make + "| Model: " + Model + " | Gender: " + Registration + " | Engine Size: " + EngineSize;
+        }
+
+    }
+}
---
+++ Fleet/Program.cs
@@ -0,0 +1,75 @@
+﻿using Fleet.Models;
+using System.Collections.Generic;
+
+namespace Fleet.Models
+{
+
+    public class Program
+    {
+        static void Main()
+        {
+           var CarList = new List<Car>
+            {
+                new Car("Honda", "Civic", "11-D-22332", 1500),
+                new Car("Hyundai", "I30", "17-WX-29732", 2100),
+                new Car("Mazda", "Swift", "07-D-290", 1800),
+                new Car("Opel", "Corsa", "02-C-1278", 1600),
+                new Car("Audi", "A4", "12-G-8788", 999),
+            };
+
+
+
+            var RegQuery = CarList
+            .OrderBy(Car => Car.Registration);
+
+            foreach (Car Details in RegQuery)
+            {
+                Console.WriteLine(Details.ToString());
+            }
+
+
+            var MazdaQuery = CarList
+            .Where(Car => Car.Make == "Mazda");
+
+            foreach (Car Details in MazdaQuery)
+            {
+                Console.WriteLine(Details.ToString());
+            }
+
+
+            var EngineSizeQuery = CarList
+            .OrderByDescending(Car => Car.EngineSize);
+
+            foreach (Car Details in EngineSizeQuery)
+            {
+                Console.WriteLine(Details.ToString());
+            }
+
+
+            var EngineSize1600Query = CarList
+                .Where(Car => Car.EngineSize == 1600);
+
+
+            foreach (Car Details in EngineSize1600Query)
+            {
+                Console.WriteLine("Make " + Details.Make + " | Model " + Details.Model);
+            }
+
+
+            var less1600Query = CarList
+                .Where(Car => Car.EngineSize < 1600);
+
+            var x = 0;
+            foreach (Car Details in EngineSize1600Query)
+            {
+                x += 1;
+            }
+            Console.WriteLine("Number of cars with engine size less than 1600cc is: " + x);
+
+
+        }
+
+
+    }
+}
+
